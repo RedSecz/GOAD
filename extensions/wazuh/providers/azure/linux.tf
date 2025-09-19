1171d3b7cab22cb126c7395745ec7285048e4798
Ren <renrror@gmail.com>
2025-09-19 13:45:13 +0700

    fix: specify password to comply with azure

--- extensions/wazuh/providers/azure/linux.tf
+++ extensions/wazuh/providers/azure/linux.tf
@@ -3,6 +3,6 @@
   linux_sku          = "22_04-lts-gen2"
   linux_version      = "latest"
   private_ip_address = "{{ip_range}}.51"
-  password           = "sgdvnkjhdshlsd"
+  password           = "sgDiuqwdh-DK12"
   size               = "Standard_B2ms"  # 2cpu/8G
 }
